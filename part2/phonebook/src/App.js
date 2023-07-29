5a2e66f8923fca1334ff736ac0e3570ef9c53936
Chris Hayduk <chayduk@fordham.edu>
2023-07-29 12:24:32 -0400

    Comitting changes

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,10 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [filteredPersons, setFilteredPersons] = useState(persons)
+  
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -20,11 +14,6 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  const handleFilterChange = (event) => {
-    const filteredData = persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase()));
-    setFilteredPersons(filteredData)
-    setNewFilter(event.target.value)
-  }
 
   const addNameNumber = (event) => {
     event.preventDefault()
@@ -46,7 +35,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      filter shown with <input value={newFilter} onChange={handleFilterChange} />
+      <Filter />
       <h3>add a new</h3>
       <form onSubmit={addNameNumber}>
         <div>
--- part2/phonebook/src/components/Filter.js
+++ part2/phonebook/src/components/Filter.js
@@ -1,5 +1,26 @@
+import { useState } from 'react'
+
+
+
+
 const Filter = () => {
+    const [filteredPersons, setFilteredPersons] = useState(persons)
+    const [newFilter, setNewFilter] = useState('')
+    const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  const handleFilterChange = (event) => {
+    const filteredData = persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase()));
+    setFilteredPersons(filteredData)
+    setNewFilter(event.target.value)
+  }
     return(<div>
         filter shown with <input value={newFilter} onChange={handleFilterChange} />
     </div>)
 }
+
+export default Filter
